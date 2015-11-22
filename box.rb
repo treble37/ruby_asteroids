3a3c4340fd9c573b146cad4154aaabd97179445e
Bruce <brucepark0@gmail.com>
2015-11-21 16:09:42 -0800

    Add white squares
    - Add white squares
    - Allow red box to move and "wrap around" screen

--- box.rb
+++ box.rb
@@ -9,9 +9,17 @@ class Box
     @color = color
   end
 
-  def move(x, y)
-    @x += x
-    @y += y
+  def move(x, y, x_boundary, y_boundary)
+    @x = (@x + x) % x_boundary
+    @y = (@y + y) % y_boundary
+  end
+
+  def randomize_location(x_boundary, y_boundary)
+    prng = Random.new
+    x_rand = prng.rand(10..x_boundary-10)
+    y_rand = prng.rand(10..y_boundary-10)
+    @x = x_rand
+    @y = y_rand
   end
 
 end
--- game_window.rb
+++ game_window.rb
@@ -1,5 +1,6 @@
 require 'gosu'
 require_relative 'box'
+require 'pry'
 
 class GameWindow < Gosu::Window
   attr_accessor :width, :height
@@ -9,22 +10,31 @@ class GameWindow < Gosu::Window
     super @width, @height
     self.caption = "Gosu Asteroids"
     @box = Box.new(width/2, height/2, 10, 10, Gosu::Color::RED)
+    @enemy_boxes = []
+    10.times do
+      box = Box.new(width/2, height/2, 5, 5, Gosu::Color::WHITE)
+      box.randomize_location(width, height)
+      @enemy_boxes << box
+    end
   end
 
   def update
     if Gosu::button_down? Gosu::KbLeft
-      @box.move(-2, 0)
+      @box.move(-2, 0, width, height)
     elsif Gosu::button_down? Gosu::KbRight
-      @box.move(2, 0)
+      @box.move(2, 0, width, height)
     elsif Gosu::button_down? Gosu::KbUp
-      @box.move(0, -2)
+      @box.move(0, -2, width, height)
     elsif Gosu::button_down? Gosu::KbDown
-      @box.move(0, 2)
+      @box.move(0, 2, width, height)
     end
   end
 
   def draw
     draw_quad(@box.x, @box.y, @box.color, @box.x, @box.y + @box.height, @box.color, @box.x + @box.width, @box.y + @box.height, @box.color, @box.x + @box.width, @box.y, @box.color, 0.0, :default)
+    @enemy_boxes.each do |enemy_box|
+      draw_quad(enemy_box.x, enemy_box.y, enemy_box.color, enemy_box.x, enemy_box.y + enemy_box.height, enemy_box.color, enemy_box.x + enemy_box.width, enemy_box.y + enemy_box.height, enemy_box.color, enemy_box.x + enemy_box.width, enemy_box.y, enemy_box.color, 0.0, :default)
+    end
   end
 end
 
