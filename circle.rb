5d7631ca3c154ed9ecd1e8410c8bde40704e47dd
Bruce <brucepark0@gmail.com>
2016-03-19 17:25:29 -0700

    Refactor game_window
    - Refactor initialize method

--- circle.rb
+++ circle.rb
@@ -41,17 +41,4 @@ class Circle
   end
 end
 
-class Bullet < Circle
-
-  def move
-    @angle = angle % 360.0
-    @x = (x + speed * Math.cos(angle*Math::PI/180.0).round(2))
-    @y = (y + speed * Math.sin(angle*Math::PI/180.0).round(2))
-  end
-
-  def onscreen?(x_boundary, y_boundary)
-    x < x_boundary && y < y_boundary
-  end
-
-end
 
--- game_window.rb
+++ game_window.rb
@@ -1,6 +1,7 @@
 require 'gosu'
 require_relative 'box'
 require_relative 'circle'
+require_relative 'bullet'
 require 'pry'
 
 class GameWindow < Gosu::Window
@@ -11,22 +12,12 @@ class GameWindow < Gosu::Window
     @height = height
     super @width, @height
     self.caption = "Gosu Asteroids"
-    ship_radius = 10
-    @circle = Circle.new(ship_radius, width, height, Gosu::Color::RED, width/2, height/2, 0.0, 270.0)
-    @ship_head = Circle.new(ship_radius/2, width, height, Gosu::Color::GREEN, width/2 + ship_radius/2, height/2 + ship_radius/2, 0.0, 270.0)
-    @img_circle = Gosu::Image.new(self, @circle, false)
-    @img_ship_head = Gosu::Image.new(self, @ship_head, false)
-    @enemy_circles = []
-    @enemy_img_circles = []
+    @ship_head, @ship_body, @img_ship_head, @img_ship_body = create_ship
+    @asteroids = []
+    @img_asteroids = []
+    @asteroids, @img_asteroids = create_asteroids
     @bullets = []
     @bullet_imgs = []
-    prng = Random.new
-    10.times do
-      circle = Circle.new(12, width, height, Gosu::Color::WHITE, 0, 0, 1.0, prng.rand(1..359))
-      circle.randomize_location(width, height)
-      @enemy_circles << circle
-      @enemy_img_circles << Gosu::Image.new(self, circle, false)
-    end
   end
 
   def update
@@ -42,7 +33,7 @@ class GameWindow < Gosu::Window
       speed_step = -0.25
     elsif Gosu::button_down? Gosu::KbSpace
       bullet_radius = 2
-      bullet = Bullet.new(bullet_radius, width, height, Gosu::Color::BLUE, @circle.x + @ship_head.radius + bullet_radius, @circle.y + @ship_head.radius + bullet_radius, @circle.speed + 2.0, @circle.angle)
+      bullet = Bullet.new(bullet_radius, width, height, Gosu::Color::BLUE, @ship_body.x + @ship_head.radius + bullet_radius, @ship_body.y + @ship_head.radius + bullet_radius, @ship_body.speed + 2.0, @ship_body.angle)
       @bullets << bullet
       @bullet_imgs << Gosu::Image.new(self, bullet, false)
     end
@@ -51,10 +42,10 @@ class GameWindow < Gosu::Window
       bullet = nil if !bullet.onscreen?(width, height)
     end
     prng_move = Random.new
-    @enemy_circles.each do |enemy_circle|
+    @asteroids.each do |enemy_circle|
       enemy_circle.move(0, 0.0, width, height)
     end
-    @circle.move(angle_step, speed_step, width, height)
+    @ship_body.move(angle_step, speed_step, width, height)
     @ship_head.move(angle_step, speed_step, width, height)
     asteroid_hit?
     close if collision?
@@ -64,20 +55,20 @@ class GameWindow < Gosu::Window
     @bullet_imgs.each_with_index do |image, i|
       image.draw @bullets[i].x, @bullets[i].y, 0, 1, 1, @bullets[i].color, :default
     end
-    @img_circle.draw @circle.x,@circle.y, 0, 1, 1, @circle.color,:default
-    @img_ship_head.draw @ship_head.x + (@circle.radius)*Math.cos(@circle.angle*Math::PI/180.0).round(2), @ship_head.y + (@circle.radius)*Math.sin(@circle.angle*Math::PI/180).round(2), 0, 1, 1, @ship_head.color,:default
-    @enemy_img_circles.each_with_index do |circle, i|
-      circle.draw @enemy_circles[i].x, @enemy_circles[i].y, 0, 1, 1, @enemy_circles[i].color, :default
+    @img_ship_body.draw @ship_body.x,@ship_body.y, 0, 1, 1, @ship_body.color,:default
+    @img_ship_head.draw @ship_head.x + (@ship_body.radius)*Math.cos(@ship_body.angle*Math::PI/180.0).round(2), @ship_head.y + (@ship_body.radius)*Math.sin(@ship_body.angle*Math::PI/180).round(2), 0, 1, 1, @ship_head.color,:default
+    @img_asteroids.each_with_index do |circle, i|
+      circle.draw @asteroids[i].x, @asteroids[i].y, 0, 1, 1, @asteroids[i].color, :default
     end
   end
 
   def collision?
     collision = false
-    @enemy_circles.each do |enemy|
-      dx = (@circle.x - enemy.x)**2
-      dy = (@circle.y - enemy.y)**2
+    @asteroids.each do |enemy|
+      dx = (@ship_body.x - enemy.x)**2
+      dy = (@ship_body.y - enemy.y)**2
       distance = Math.sqrt(dx+dy)
-      if (distance < (@circle.radius + enemy.radius))
+      if (distance < (@ship_body.radius + enemy.radius))
         collision = true
       end
       break if collision
@@ -88,7 +79,7 @@ class GameWindow < Gosu::Window
   def asteroid_hit?
     hit = false
     delete_index = nil
-    @enemy_circles.each_with_index do |enemy, i|
+    @asteroids.each_with_index do |enemy, i|
       @bullets.each do |bullet|
         dx = (bullet.x - enemy.x)**2
         dy = (bullet.y - enemy.y)**2
@@ -100,28 +91,52 @@ class GameWindow < Gosu::Window
       end
     end
     if delete_index
-      @enemy_circles.delete_at(delete_index)
-      @enemy_img_circles.delete_at(delete_index)
+      @asteroids.delete_at(delete_index)
+      @img_asteroids.delete_at(delete_index)
     end
     hit
   end
 
   def asteroid_split_or_delete_index(i)
     min_delete_radius = 4
-    new_enemy_circle_radius = @enemy_circles[i].radius / 2
+    new_enemy_circle_radius = @asteroids[i].radius / 2
     if (new_enemy_circle_radius > min_delete_radius)
-      circle_x = @enemy_circles[i].x
-      circle_y = @enemy_circles[i].y
-      circle_speed = @enemy_circles[i].speed
-      circle_angle = @enemy_circles[i].angle + 90
+      circle_x = @asteroids[i].x
+      circle_y = @asteroids[i].y
+      circle_speed = @asteroids[i].speed
+      circle_angle = @asteroids[i].angle + 90
       (0..1).each do |j|
         mult = (j%2==0) ? 1 : -1
         circle = Circle.new(new_enemy_circle_radius, width, height, Gosu::Color::WHITE, circle_x, circle_y, circle_speed, mult*circle_angle)
-        @enemy_circles << circle
-        @enemy_img_circles << Gosu::Image.new(self, circle, false)
+        @asteroids << circle
+        @img_asteroids << Gosu::Image.new(self, circle, false)
       end
     end
     i
+  end
+
+  private
+
+  def create_ship
+    ship_radius = 10
+    ship_body = Circle.new(ship_radius, width, height, Gosu::Color::RED, width/2, height/2, 0.0, 270.0)
+    ship_head = Circle.new(ship_radius/2, width, height, Gosu::Color::GREEN, width/2 + ship_radius/2, height/2 + ship_radius/2, 0.0, 270.0)
+    img_circle = Gosu::Image.new(self, ship_body, false)
+    img_ship_head = Gosu::Image.new(self, ship_head, false)
+    [ship_head, ship_body, img_ship_head, img_circle]
+  end
+
+  def create_asteroids
+    prng = Random.new
+    asteroids = []
+    img_asteroids = []
+    10.times do
+      circle = Circle.new(12, width, height, Gosu::Color::WHITE, 0, 0, 1.0, prng.rand(1..359))
+      circle.randomize_location(width, height)
+      asteroids << circle
+      img_asteroids << Gosu::Image.new(self, circle, false)
+    end
+    [asteroids, img_asteroids]
   end
 
 end
